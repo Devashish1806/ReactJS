2d68ea09a27993fb7b9c31c2ebeec9097a73259e
Devashish Mali <devashish.mali06@gmail.com>
2022-08-22 15:16:36 +0530

    composition added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
-  const expenses = [
+  const expenses = getExpenses();
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses expenses={expenses}></Expenses>
+    </div>
+  );
+}
+
+function getExpenses() {
+  return [
     {
       id: 'e1',
       title: 'Paper',
@@ -22,31 +32,6 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
-    </div>
-  );
 }
 
 export default App;
