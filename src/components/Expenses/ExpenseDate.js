917d9f035a7b1a95d774b9f809008a5d639930bf
Devashish Mali <devashish.mali06@gmail.com>
2022-08-22 18:26:11 +0530

    state hooks added onClick

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,13 +1,16 @@
+import Card from '../UI/Card';
+import './ExpenseDate.css';
+
 function ExpenseDate(props) {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const date = props.date.toLocaleString('en-US', { day: 'numeric' });
+  const day = props.date.toLocaleString('en-US', { day: 'numeric' });
   const year = props.date.toLocaleString('en-US', { year: 'numeric' });
   return (
-    <div>
-      <div>{month}</div>
-      <div>{date}</div>
-      <div>{year}</div>
-    </div>
+    <Card className="date">
+      <div id="Expense-Date__month">{month}</div>
+      <div id="Expense-Date__year">{day}</div>
+      <div id="Expense-Date__year">{year}</div>
+    </Card>
   );
 }
 
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  // first is the property name and the second is the function attached to it for updating the state.
+  // use state will reexcute the particular component inside chich it is called not all components are executed again.
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle('Updated');
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
