8c21f4e7826d57c4a6b62fb3e253928ba648811b
Devashish Mali <devashish.mali06@gmail.com>
2022-08-22 19:51:23 +0530

    add expense form added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = getExpenses();
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense></NewExpense>
       <Expenses expenses={expenses}></Expenses>
     </div>
   );
