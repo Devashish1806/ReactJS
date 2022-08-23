935c8f8efa7de52b787363c4239f039d3fea7eed
Devashish Mali <devashish.mali06@gmail.com>
2022-08-23 19:00:12 +0530

    rendering list completed

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import './Expenses.css';
 
 function Expenses(props) {
   const [enteredYear, setEnteredYear] = useState('2020');
-  const filteredItems = props.items.filter(
+  const filteredList = props.items.filter(
     (expense) =>
       enteredYear === expense.date.toLocaleString('en-US', { year: 'numeric' }),
   );
@@ -15,20 +15,15 @@ function Expenses(props) {
     console.log(selectedYear);
   };
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        onChangeFilter={changeFilterHandler}
-        selectedYear={enteredYear}
-      ></ExpensesFilter>
-      {filteredItems.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-    </Card>
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          onChangeFilter={changeFilterHandler}
+          selectedYear={enteredYear}
+        ></ExpensesFilter>
+        <ExpensesList list={filteredList} />
+      </Card>
+    </li>
   );
 }
 
