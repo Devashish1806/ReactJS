315ac26aabdcec25b346d4c2b9cb7c3c62c00ebe
Devashish Mali <devashish.mali06@gmail.com>
2022-08-24 19:16:14 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,45 +1,31 @@
 import React, { useState } from 'react';
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  const [expenses, setExpenses] = useState(getExpenses());
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    });
-  };
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses items={expenses} />
-    </div>
-  );
-}
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-function getExpenses() {
-  return [
-    {
-      id: 'e1',
-      title: 'Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'Desk',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
