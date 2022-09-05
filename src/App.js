da3b8294a54ef7d2cd91d599b38c35aceabf6f39
Devashish Mali <devashish.mali06@gmail.com>
2022-09-05 23:13:15 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import React, { useContext } from 'react';
+import Counter from './components/Counter';
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './context/auth-context';
 
 function App() {
-  const authCtx = useContext(AuthContext);
   return (
-    <React.Fragment>
-      <MainHeader />
-      <main>
-        {!authCtx.isLoggedIn && <Login />}
-        {authCtx.isLoggedIn && <Home />}
-      </main>
-    </React.Fragment>
+    <Counter />
   );
 }
 
