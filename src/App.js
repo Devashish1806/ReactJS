ed22241f7737b20b23cff449e4ca141a7edf797d
Devashish Mali <devashish.mali06@gmail.com>
2023-09-29 11:10:03 +0530

    Side effect reducer context (#1)
    * initial setup
    
    * localstorage and useEffect hook
    
    * useReducer completed
    
    * added auth context
    
    * added custom auth context and proder with state management

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
-import React, { useState } from 'react';
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
+import React, { useContext } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './context/auth-context';
 
 function App() {
-  const [expenses, setExpenses] = useState(getExpenses());
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    });
-  };
+  const authCtx = useContext(AuthContext);
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses items={expenses} />
-    </div>
+    <React.Fragment>
+      <MainHeader />
+      <main>
+        {!authCtx.isLoggedIn && <Login />}
+        {authCtx.isLoggedIn && <Home />}
+      </main>
+    </React.Fragment>
   );
-}
-
-function getExpenses() {
-  return [
-    {
-      id: 'e1',
-      title: 'Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'Desk',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
 }
 
 export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect, useReducer, useContext } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+import AuthContext from '../../context/auth-context';
+
+const emailReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.value, isValid: action.value.includes('@') };
+  }
+  if (action.type === 'BLUR_INPUT') {
+    return { value: state.value, isValid: state.value.includes('@') };
+  }
+  return { value: '', isValid: false };
+};
+
+const passReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.value, isValid: action.value.trim().length > 6 };
+  }
+  if (action.type === 'BLUR_INPUT') {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+  return { value: '', isValid: false };
+};
+
+const Login = (props) => {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: null,
+  });
+
+  const [passState, dispatchPass] = useReducer(passReducer, {
+    value: '',
+    isValid: null,
+  });
+
+  const authCtx = useContext(AuthContext);
+
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passIsValid } = passState;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log('EFFECT');
+      setFormIsValid(emailIsValid && passIsValid);
+    }, 500);
+    return () => {
+      console.log('CLEANUP');
+      clearTimeout(timer);
+    };
+  }, [emailIsValid, passIsValid]);
+
+  const emailChangeHandler = (event) => {
+    dispatchEmail({ type: 'USER_INPUT', value: event.target.value });
+  };
+
+  const passwordChangeHandler = (event) => {
+    dispatchPass({ type: 'USER_INPUT', value: event.target.value });
+  };
+
+  const validateEmailHandler = () => {
+    dispatchEmail({ type: 'BLUR_INPUT' });
+  };
+
+  const validatePasswordHandler = () => {
+    dispatchPass({ type: 'BLUR_INPUT' });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    authCtx.onLogin(emailState.value, passState.value);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailState.isValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={emailState.value}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passState.isValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={passState.value}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
